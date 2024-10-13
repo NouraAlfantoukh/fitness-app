521ad5c79b2362728c43c6908e441f3b59166547
NouraAlfantoukh <105815363+NouraAlfantoukh@users.noreply.github.com>
2024-10-13 03:20:32 +0300

    Update ImageSlider.jsx
    error code so i commented it

--- components/ImageSlider.jsx
+++ components/ImageSlider.jsx
@@ -1,10 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+//original code but it has style error that won't solve!!
 
-export default function ImageSlider() {
-  return (
-    <View>
-      <Text>ImageSlider</Text>
-    </View>
-  )
-}
+
+// import { View, Text } from 'react-native'
+// import React from 'react'
+// import Carousel,{ParallaxImage} from 'react-native-snap-carousel';
+// import { sliderImages } from '../constants';
+// import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+
+
+
+// export default function ImageSlider() {
+//   return (
+//     <Carousel
+//     data={sliderImages}
+//     loop={true}
+//     autoplay={true}
+//     renderItem={ItemCard}
+//     hasParallaxImages={true}
+//     sliderWidth={wp(100)}
+//     firstItem={1}
+//     autoplayInterval={4000}
+//     itemWidth={wp(100)-70}
+//     slideStyle={{display:'flex', alignItems:'center'}}
+
+
+
+    
+    
+//     />
+//   )
+
+  
+  
+// }
+
+
+// const ItemCard = ({item, index}, ParallaxImage)=>{
+//     return(
+//         <View style={{width: wp(100)-70, height: hp(25)}}>
+
+// <ParallaxImage
+// source ={item}
+// containerStyle={{borderRadius: 30, flex:1}}
+// style={{resizeMode: 'contain'}}
+// parallaxFactor={1}
+// {...parallaxProps}
+
+// />
+
+//         </View>
+//     )
+
+//   }
+
+
